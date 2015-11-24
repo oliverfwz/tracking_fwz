0627484e32f1c72466ce43ae6256d3a0d7bbf7e1
Oliver Futureworkz <oliver@futureworkz.com>
2015-11-24 22:58:22 +0700

    Build method tracking_send_mail

--- lib/generators/tracking/install/templates/controllers/concerns/tracking_concern.rb
+++ lib/generators/tracking/install/templates/controllers/concerns/tracking_concern.rb
@@ -1,3 +1,8 @@
 module TrackingConcern
   extend ActiveSupport::Concerns
+
+  def tracking_send_mail( model, action, options)
+    options[:cookies] = cookies[:tracking_history]
+    model.send(action, options).deliver_now
+  end
 end

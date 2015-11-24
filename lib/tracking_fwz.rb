1760922c294f98fd742a846562a0b0f21fb920ba
Oliver Futureworkz <oliver@futureworkz.com>
2015-11-25 00:21:07 +0700

    Update module, defined model_devise for config

--- lib/tracking_fwz.rb
+++ lib/tracking_fwz.rb
@@ -1,5 +1,13 @@
 require "tracking_fwz/version"
 
 module TrackingFwz
-  # Your code goes here...
+  class Engine < ::Rails::Engine
+  end
+
+  mattr_accessor :model_devise
+  @@model_devise = nil
+
+  def self.setup
+    yield self
+  end
 end
